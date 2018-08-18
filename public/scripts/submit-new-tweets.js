2d517d6fcb066ae6c309b9ee09c9a3583acb7084
mag <mag.it.use@gmail.com>
2018-08-18 00:02:22 +0000

    added tweet contents validation. The user will be given an error that their tweet content is too long or that it is not present.

--- public/scripts/submit-new-tweets.js
+++ public/scripts/submit-new-tweets.js
@@ -1,13 +1,18 @@
 $(document).ready(function() {
   $("#new-tweet-submit").on("click", function(event) {
     event.preventDefault();
-    let $rawTweetText = $("#new-tweet-content").serialize();
-    console.log($rawTweetText);
-    $("#tweets-container").empty();
-    $("#new-tweet-content").val("");
-    $(".counter").text("140");
-    $.post("/tweets", $rawTweetText, function (){
-      loadPage();
-    });
+    if ($("#new-tweet-content").val() === "") {
+      alert ("Please fill out the content in your tweet!");
+    } else if ($(".new-tweet textarea").val().length > 140) {
+      alert ("Seems like your tweet content is too long.. Please limited it within 140 characters.");
+    } else {
+      let $rawTweetText = $("#new-tweet-content").serialize();
+      $("#tweets-container").empty();
+      $("#new-tweet-content").val("");
+      $(".counter").text("140");
+      $.post("/tweets", $rawTweetText, function (){
+        loadTweets();
+      });
+    }
   });
 });
