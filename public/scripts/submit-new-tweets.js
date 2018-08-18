8ddc1c84d541c0dd90daf99994d35e0d662ad60b
mag <mag.it.use@gmail.com>
2018-08-18 18:56:56 +0000

    replaced the 'alert call' error msg with an error msg box above the new tweet box by use 'jQuery call', when user want to submit a tweet with no content or longer than 140 char, error msg will show. when user reclick the new tweet box, the error msg hide

--- public/scripts/submit-new-tweets.js
+++ public/scripts/submit-new-tweets.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
+  $("#new-tweet-content").on("click", function(event){
+    $("#error-msg").slideUp();
+  });
+
   $("#new-tweet-submit").on("click", function(event) {
     event.preventDefault();
     if ($("#new-tweet-content").val() === "") {
-      alert ("Please fill out the content in your tweet!");
+      $("#error-msg").text("Please fill out the content in your tweet!").slideDown();
     } else if ($(".new-tweet textarea").val().length > 140) {
-      alert ("Seems like your tweet content is too long.. Please limited it within 140 characters.");
+      $("#error-msg").text("Seems like your tweet content is too long. Please limited it within 140 characters.").slideDown();
     } else {
       let $rawTweetText = $("#new-tweet-content").serialize();
       $("#tweets-container").empty();
